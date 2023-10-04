2ac09073f24fd22584c07d4afc626f4b30e56adf
kawatech002 <144366515+kawatech002@users.noreply.github.com>
2023-10-04 15:59:52 +0300

    Update salary.js challenge updated
    corrections on the net salary calculate

--- toy-problems/salary.js
+++ toy-problems/salary.js
@@ -1,51 +1,137 @@
 
-// Step-by-step explanation:
+// net salary calculator step by step explanation 
 
-// Calculate the gross salary.
-// Calculate the PAYE tax.
-// Calculate the NHIF deduction.
-// Calculate the NSSF deduction.
-// Calculate the net salary
+// Write a program whose major task is to calculate an individual’s Net Salary by getting the inputs of basic salary and benefits.
+//  Calculate the payee (i.e. Tax), NHIF Deductions, NSSF Deductions, gross salary, and net salary. 
 
-// Define the net salary function passing the two arguments basic salary and benefits. 
-function computeNetSalary(basicSalary, benefits) {
-    const grossSalary = basicSalary + benefits;
-    // //we now calculate NHIF deductuion 
-    // NHIF deductions with a pay of between 0 and 5000
-    if (grossSalary >= 0 && grossSalary <= 5,999) { return (grossSalary - 150);}
-    else if (grossSalary >= 6000 && grossSalary <= 7999) {return (grossSalary - 300);}
-    else if (grossSalary >= 8000 && grossSalary <= 11999) {return (grossSalary - 400);}
-    else if (grossSalary >= 12000 && grossSalary <= 14999) {return (grossSalary - 500);}
-    else if (grossSalary >= 15000 && grossSalary <= 19999) {return (grossSalary - 600);}
-    else if (grossSalary >= 20000 && grossSalary <= 24999) {return (grossSalary - 750);}
-    else if (grossSalary >= 25000 && grossSalary <= 29999) {return (grossSalary - 850);}
-    else if (grossSalary >= 30000 && grossSalary <= 34999) {return (grossSalary - 900); }
-    else if (grossSalary >= 35000 && grossSalary <= 39999) {return (grossSalary - 950);}
-    else if (grossSalary >= 40000 && grossSalary <= 44999) {return (grossSalary - 1000);}
-    else if (grossSalary >= 45000 && grossSalary <= 49999) {return (grossSalary - 1100);}
-    else if (grossSalary >= 50000 && grossSalary <= 59999) {return (grossSalary - 1200);}
-    else if (grossSalary >= 60000 && grossSalary <= 69999) {return (grossSalary - 1300);}
-    else if (grossSalary >= 70000 && grossSalary <= 79999) {return (grossSalary - 1400);}
-    else if (grossSalary >= 80000 && grossSalary <= 89999) {return (grossSalary - 1500);}
-    else if (grossSalary >= 90000 && grossSalary <= 99999) {return (grossSalary - 1600);}
-    else if (grossSalary >= 100000) {return (grossSalary - 1700);}
-}
-  // Calculate the PAYE (Pay As You Earn) tax.
-  const payeeDeductions= function () {
-    if (grossSalary >=1 && grossSalary <= 24000) { return grossSalary * 0.1} 
-    else if (grossSalary >= 24001 && grossSalary >= 32333) { return grossSalary * 0.25}
-    else if (grossSalary >= 32334 && grossSalary >= 500000) { return grossSalary * 0.30}
-    else if (grossSalary >= 500001 && grossSalary >= 800000) { return grossSalary * 0.325}
-    else if (grossSalary > 800000) { return grossSalary * 0.35}
-}
-// Calculate the NSSF (National Social Security Fund) deduction.
-const nssfDeductions = function () {
-if (grossSalary >= 1 && grossSalary <= 6000){return grossSalary * 0.06}
-else if (grossSalary >= 6001){ return grossSalary*0.06}
-}
-// Returning the net income from above deductions
-return (afterNhifDeductions() - (payeeDeductions() + nssfDeductions()));
+// NB: Use KRA, NHIF, and NSSF values provided in the link below.
 
-const basicSalary = 100000;
-const benefits = 1000;
-console.log(computeNetSalary(basicSalary, benefits));
+// - https://www.aren.co.ke/payroll/taxrates.htm Links to an external site.
+
+// Links to an external site.-  Links to an external site.Links to an external site.
+
+// - www.kra.go.ke/en/individual/calculate-tax/calculating-tax/payeLinks to an external site.
+
+
+const taxRates = [{
+  // the tax rate for salary between 0 to 24000
+  min:0,
+  max:24000,
+  percentageRate: 0.1
+}, 
+// the rate for slaraly between 24001 to 32333
+{
+  min:24001,
+  max:32333,
+  percentageRate: 0.25
+}, 
+// the taxe rate for salary  between 32334 to 50000
+{
+  min:32334,
+  max:500000,
+  percentageRate: 0.3
+}, 
+// the tax rate for salary between 500001 to 800000
+{
+  min:500001,
+  max:800000,
+  percentageRate: 0.325
+}, 
+
+// the tax rates for the salary between 800001 to a maximum of infinity 
+{
+  min:800001,
+  max:Infinity,
+  percentageRate: 0.35
+}];
+
+// we now define the nhif deductions as follows
+const nhif = [{
+  // with a gross salary of 0 to 5999 
+  grossPayMin: 0,
+  grossPayMax: 5999,
+  deduction: 150
+}, 
+
+// with a gross salary of 6000 to 7999
+{
+  grossPayMin:6000,
+  grossPayMax:7999,
+  deduction:300
+}, 
+
+// with a gross salary of 8000 to 11999
+{
+  grossPayMin:8000,
+  grossPayMax:11999,
+  deduction:400
+}, 
+
+// with a gross salary of 12000 to 14999
+{
+  grossPayMin:12000,
+  grossPayMax:14999,
+  deduction:500
+}, 
+
+// with a gross salary of 15000 to 19999
+{
+  grossPayMin:15000,
+  grossPayMax:19999,
+  deduction:600
+}
+
+];
+// here we calculate netsalary as follows 
+const netSalary =(basicSalary, benefits)=>{
+  // lets get the taxable income
+  let tax;
+  let nhifRate;
+  
+      for (let i = 0; i < taxRates.length; i++) {
+          if (basicSalary >= taxRates[i].min && basicSalary <= taxRates[i].max) {
+              // console.log(taxRates[i].percentageRate)
+              tax = taxRates[i].percentageRate * basicSalary;
+             
+              // return taxRates[i].percentageRate;
+          }
+      }
+      // return null; 
+      // takes cre of the case where basicSalary is outside all tax brackets
+  
+      // console.log(`tax is ${tax}`)
+      ;
+nssfRate = 200;
+
+//find nhif payable
+
+    for (let i = 0; i < nhif.length; i++) {
+        // console.log('running');
+        if (basicSalary >= nhif[i].grossPayMin && basicSalary <= nhif[i].grossPayMax) {
+            // console.log(nhif[i].deduction);
+            nhifRate= nhif[i].deduction;
+        }
+    }
+    // return null; // Handle the case where basicSalary is outside all ranges
+
+    // console.log(`payee: ${tax}`)
+    // console.log(`nhif: ${nhifRate}`)
+    // console.log(`nssf: ${nssfRate}`)
+   
+const totalDeductions =  tax + nhifRate + nssfRate;
+
+// console.log(`total deduction is ${totalDeductions}`)
+
+const grosfPay = basicSalary + benefits;
+
+const netSalary = grosfPay -totalDeductions;
+
+// console.log(`net pay  is ${netSalary}`)
+return {netSalary, nssfRate, nhifRate, tax};
+}
+
+
+//  tax(10000, 2500)
+
+ console.log( netSalary(10000, 2500));
+  
